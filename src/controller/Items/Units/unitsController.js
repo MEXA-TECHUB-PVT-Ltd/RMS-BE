b02039857002f9899cc9bf5e597849a4633069a6
mahreentassawar@mtechub.org <mahreentassawar@gmail.org>
2024-09-05 10:48:26 +0500

    final test

---
+++ src/controller/Items/Units/unitsController.js
@@ -0,0 +1,184 @@
+const pool = require("../../../config/db");
+const { responseSender } = require("../../../utilities/responseHandlers");
+const { pagination } = require('../../../utilities/pagination');
+
+const validUnits = {
+    mass: ['kg', 'g', 'mg'],
+    volume: ['l', 'ml'],
+    quantity: ['packs', 'bottles', 'dozen'] // Any number can be given as a unit
+};
+
+const createUnits = async (req, res, next) => {
+    const { category, unit, quantity, usage_quantity } = req.body;
+
+    if (!category || !unit) {
+        return responseSender(res, 422, false, "Category and unit are required");
+    }
+
+    const validCategories = Object.keys(validUnits);
+    if (!validCategories.includes(category)) {
+        return responseSender(res, 422, false, "Invalid category. Must be 'mass', 'liquid', or 'quantity'");
+    }
+
+    // if (category === 'quantity') {
+    //     // Check if the unit is valid for the 'quantity' category
+    //     if (!validUnits[category].includes(unit)) {
+    //         return responseSender(res, 422, false, `Invalid unit for category '${category}'. Valid units are: ${validUnits[category].join(', ')}`);
+    //     }
+
+    //     // Validate the quantity and usage_quantity
+    //     const quantityAsNumber = Number(quantity);
+    //     const usageQuantityAsNumber = Number(usage_quantity);
+
+    //     if (!quantity || isNaN(quantityAsNumber) || quantityAsNumber <= 0) {
+    //         return responseSender(res, 422, false, "Invalid quantity. Quantity must be a positive number.");
+    //     }
+
+    //     if (!usage_quantity || isNaN(usageQuantityAsNumber) || usageQuantityAsNumber <= 0) {
+    //         return responseSender(res, 422, false, "Invalid usage quantity. Usage quantity must be a positive number.");
+    //     }
+    // } else if (!validUnits[category].includes(unit)) {
+    //     return responseSender(res, 422, false, `Invalid unit for category '${category}'. Valid units are: ${validUnits[category].join(', ')}`);
+    // }
+
+    try {
+        // Insert the unit into the database
+        const result = await pool.query(
+            'INSERT INTO units (category, unit, quantity, usage_quantity) VALUES ($1, $2, $3, $4) RETURNING *',
+            [category, unit, category === 'quantity' ? quantity : null, category === 'quantity' ? usage_quantity : null]
+        );
+
+        return responseSender(res, 201, true, "Unit Added", result.rows[0]);
+    } catch (error) {
+        next(error);
+    }
+};
+
+const unitsList = async (req, res, next) => {
+    try {
+        // Fetch all units
+        const result = await pool.query('SELECT * FROM units');
+
+        // Get the units as a single array
+        const units = result.rows;
+
+        // Count total units
+        const totalUnits = units.length;
+
+        return responseSender(res, 200, true, "Units fetched", {
+            count: totalUnits,
+            units: units
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const getUnitsByCategory = async (req, res, next) => {
+    const { category } = req.query;
+    const perPage = Number.parseInt(req.query.perPage) || 10;
+    const currentPage = Number.parseInt(req.query.currentPage) || 1;
+
+    // Validate category
+    if (!validUnits[category]) {
+        return responseSender(res, 422, false, "Invalid category, Category can be only mass, volume and quantity");
+    }
+
+    try {
+        // Fetch total count of items for the given category
+        const countResult = await pool.query('SELECT COUNT(*) FROM units WHERE category = $1', [category]);
+        const totalItems = Number.parseInt(countResult.rows[0].count);
+
+        // Calculate offset for pagination
+        const offset = (currentPage - 1) * perPage;
+
+        // Fetch paginated data
+        const result = await pool.query('SELECT * FROM units WHERE category = $1 LIMIT $2 OFFSET $3', [category, perPage, offset]);
+
+        // Generate pagination info
+        const paginationInfo = pagination(totalItems, perPage, currentPage);
+
+        return responseSender(res, 200, true, "Units fetched", { count: paginationInfo.totalItems, units: result.rows });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const updateUnit = async (req, res, next) => {
+    const { id, category, unit, quantity, usage_quantity } = req.body;
+
+    if (!category || !unit) {
+        return responseSender(res, 422, false, "Category and unit are required");
+    }
+
+    const validCategories = Object.keys(validUnits);
+    if (!validCategories.includes(category)) {
+        return responseSender(res, 422, false, "Invalid category. Must be 'mass', 'liquid', or 'quantity'");
+    }
+
+    // if (category === 'quantity') {
+    //     // Check if the unit is valid for the 'quantity' category
+    //     if (!validUnits[category].includes(unit)) {
+    //         return responseSender(res, 422, false, `Invalid unit for category '${category}'. Valid units are: ${validUnits[category].join(', ')}`);
+    //     }
+
+    //     // Validate the quantity and usage_quantity
+    //     const quantityAsNumber = Number(quantity);
+    //     const usageQuantityAsNumber = Number(usage_quantity);
+
+    //     if (!quantity || isNaN(quantityAsNumber) || quantityAsNumber <= 0) {
+    //         return responseSender(res, 422, false, "Invalid quantity. Quantity must be a positive number.");
+    //     }
+
+    //     if (!usage_quantity || isNaN(usageQuantityAsNumber) || usageQuantityAsNumber <= 0) {
+    //         return responseSender(res, 422, false, "Invalid usage quantity. Usage quantity must be a positive number.");
+    //     }
+    // } else if (!validUnits[category].includes(unit)) {
+    //     return responseSender(res, 422, false, `Invalid unit for category '${category}'. Valid units are: ${validUnits[category].join(', ')}`);
+    // }
+
+    try {
+        // Validate unit ID
+        const unitResult = await pool.query('SELECT * FROM units WHERE id = $1', [id]);
+        if (unitResult.rowCount === 0) {
+            return responseSender(res, 404, false, "Unit not found");
+        }
+
+        // Update the unit
+        const result = await pool.query(
+            'UPDATE units SET category = $1, unit = $2, quantity = $3, usage_quantity = $4, updated_at = NOW() WHERE id = $5 RETURNING *',
+            [category, unit, category === 'quantity' ? quantity : null, category === 'quantity' ? usage_quantity : null, id]
+        );
+
+        return responseSender(res, 200, true, "Unit Updated", result.rows[0]);
+    } catch (error) {
+        next(error);
+    }
+};
+
+const deleteUnit = async (req, res, next) => {
+    const { id } = req.query;
+
+    try {
+        // Validate unit ID
+        const unitResult = await pool.query('SELECT * FROM units WHERE id = $1', [id]);
+        if (unitResult.rowCount === 0) {
+            return responseSender(res, 404, false, "Unit not found");
+        }
+
+        // Delete the unit
+        await pool.query('DELETE FROM units WHERE id = $1', [id]);
+
+        return responseSender(res, 200, true, "Unit deleted successfully");
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = {
+    createUnits,
+    unitsList,
+    getUnitsByCategory,
+    updateUnit,
+    deleteUnit
+};
---
+++ src/controller/Items/itemController.js
@@ -0,0 +1,458 @@
+const { v4: uuidv4 } = require('uuid');
+const pool = require("../../config/db");
+const { responseSender } = require("../../utilities/responseHandlers");
+const { pagination } = require('../../utilities/pagination');
+
+const createItem = async (req, res, next) => {
+    const {
+        type, name, product_category, unit_category, quantity_units, product_units, product_catalog,
+        usage_unit, image, vendor_ids, description, stock_in_hand,
+        opening_stock_rate, reorder_unit, inventory_description
+    } = req.body;
+
+    if (type !== 'PRODUCT' && type !== 'SERVICE') {
+        return responseSender(res, 422, false, "Invalid type. Must be PRODUCT or SERVICE.");
+    }
+
+    // if (type === 'PRODUCT') {
+    //     if (!name || !product_category || !product_units || !product_catalog || !usage_unit || !vendor_ids || vendor_ids.length === 0) {
+    //         return responseSender(res, 422, false, "Missing required product attributes.");
+    //     }
+    // } else if (type === 'SERVICE') {
+    //     if (!name || !vendor_ids || vendor_ids.length === 0 || !description) {
+    //         return responseSender(res, 422, false, "Missing required service attributes.");
+    //     }
+    // }
+
+    try {
+        const id = uuidv4();
+
+        await pool.query(
+            `INSERT INTO item (id, type, name, product_category, unit_category, quantity_units, product_units, usage_unit, product_catalog, description, stock_in_hand, opening_stock_rate, reorder_unit, inventory_description, image) 
+            VALUES ($1, $2, $3, $4::uuid, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15) RETURNING *`,
+            [id, type, name, product_category, unit_category, quantity_units, product_units, usage_unit, product_catalog, description, stock_in_hand, opening_stock_rate, reorder_unit, inventory_description, image]
+        );
+
+        for (const vendor_id of vendor_ids) {
+            await pool.query(
+                'INSERT INTO item_preferred_vendor (item_id, vendor_id) VALUES ($1::uuid, $2::uuid)',
+                [id, vendor_id]
+            );
+        }
+
+        const itemDetails = await pool.query(
+            `SELECT 
+                i.id,
+                i.type,
+                i.name AS name,
+                i.unit_category, 
+                i.product_catalog,
+                i.description,
+                i.image, 
+                i.stock_in_hand,
+                i.opening_stock_rate,
+                i.reorder_unit,
+                i.inventory_description,
+                pc.name AS product_category,
+                i.quantity_units,
+                u1.unit AS product_units,
+                u2.unit AS usage_unit,
+                json_agg(v.*) AS vendors,
+                i.created_at,
+                i.updated_at
+            FROM item i
+            LEFT JOIN product_category pc ON i.product_category = pc.id::uuid
+            LEFT JOIN units u1 ON i.product_units = u1.unit
+            LEFT JOIN units u2 ON i.usage_unit = u2.unit
+            LEFT JOIN item_preferred_vendor ipv ON i.id = ipv.item_id::uuid
+            LEFT JOIN vendor v ON ipv.vendor_id = v.id::uuid
+            WHERE i.id = $1::uuid
+            GROUP BY i.id, pc.name, u1.unit, u2.unit`,
+            [id]
+        );
+
+        return responseSender(res, 201, true, "Item Added", itemDetails.rows[0]);
+    } catch (error) {
+        next(error);
+    }
+};
+
+const itemList = async (req, res, next) => {
+    const perPage = Number.parseInt(req.query.perPage) || 10;
+    const currentPage = Number.parseInt(req.query.currentPage) || 1;
+    const searchName = req.query.name || '';
+    const searchCategory = req.query.product_category || '';
+    const searchVendorName = req.query.vendor_name || '';
+    const searchProductCatalog = req.query.product_catalog || '';
+
+    try {
+        // Construct the base query
+        let countQuery = `
+            SELECT COUNT(*) 
+            FROM item i 
+            LEFT JOIN product_category pc ON i.product_category = pc.id 
+            LEFT JOIN item_preferred_vendor ipv ON i.id = ipv.item_id 
+            LEFT JOIN vendor v ON ipv.vendor_id = v.id`;
+
+        let fetchQuery = `
+            SELECT 
+                i.id,
+                i.type,
+                i.name AS name,
+                i.product_catalog,
+                i.description,
+                i.image, 
+                pc.name AS product_category,
+                i.quantity_units,
+                i.product_units, -- Updated this to TEXT
+                i.usage_unit, -- Updated this to TEXT
+                i.stock_in_hand,
+                i.opening_stock_rate,
+                i.reorder_unit,
+                i.inventory_description,
+                json_agg(v.*) AS vendors,
+                i.created_at,
+                i.updated_at
+            FROM item i
+            LEFT JOIN product_category pc ON i.product_category = pc.id
+            LEFT JOIN item_preferred_vendor ipv ON i.id = ipv.item_id
+            LEFT JOIN vendor v ON ipv.vendor_id = v.id`;
+
+        // Add search conditions if name, product_category, vendor_name, or product_catalog are provided
+        let queryParams = [];
+        let whereClauses = [];
+
+        if (searchName) {
+            whereClauses.push(`i.name ILIKE $${queryParams.length + 1}`);
+            queryParams.push(`%${searchName}%`);
+        }
+
+        if (searchCategory) {
+            whereClauses.push(`pc.name ILIKE $${queryParams.length + 1}`);
+            queryParams.push(`%${searchCategory}%`);
+        }
+
+        if (searchVendorName) {
+            whereClauses.push(`v.vendor_display_name ILIKE $${queryParams.length + 1}`);
+            queryParams.push(`%${searchVendorName}%`);
+        }
+
+        if (searchProductCatalog) {
+            whereClauses.push(`i.product_catalog = $${queryParams.length + 1}`);
+            queryParams.push(searchProductCatalog);
+        }
+
+        if (whereClauses.length > 0) {
+            const whereClause = whereClauses.join(' AND ');
+            countQuery += ` WHERE ${whereClause}`;
+            fetchQuery += ` WHERE ${whereClause}`;
+        }
+
+        // Add GROUP BY clause to fetch query
+        fetchQuery += ` GROUP BY i.id, pc.name`;
+
+        // Add ORDER BY clause for sorting
+        fetchQuery += ` ORDER BY i.created_at DESC`;
+
+        // Calculate offset for pagination
+        const offset = (currentPage - 1) * perPage;
+
+        // Fetch paginated data
+        let result;
+        if (queryParams.length > 0) {
+            result = await pool.query(fetchQuery + ' LIMIT $' + (queryParams.length + 1) + ' OFFSET $' + (queryParams.length + 2), [...queryParams, perPage, offset]);
+        } else {
+            result = await pool.query(fetchQuery + ' LIMIT $1 OFFSET $2', [perPage, offset]);
+        }
+
+        // Fetch the total count of items
+        const countResult = await pool.query(countQuery, queryParams);
+        const totalItems = parseInt(countResult.rows[0].count, 10);
+
+        // Generate pagination info
+        const paginationInfo = pagination(totalItems, perPage, currentPage);
+
+        return responseSender(res, 200, true, "Item List fetched", { count: paginationInfo.totalItems, items: result.rows });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const specifiItem = async (req, res, next) => {
+    const itemId = req.query.id;
+
+    if (!itemId) {
+        return responseSender(res, 400, false, "Item ID is required");
+    }
+
+    try {
+        const itemQuery = `
+            SELECT 
+                i.id,
+                i.type,
+                i.name AS name,
+                i.unit_category AS unit_category,
+                i.product_catalog,
+                i.description,
+                i.image, 
+                pc.name AS product_category, 
+                i.unit_category,
+                i.quantity_units,
+                CASE 
+                    WHEN i.unit_category = 'quantity' THEN i.product_units
+                    ELSE pu.unit
+                END AS product_units,
+                CASE 
+                    WHEN i.unit_category = 'quantity' THEN i.usage_unit
+                    ELSE uu.unit
+                END AS usage_unit,
+                i.stock_in_hand,
+                i.opening_stock_rate,
+                i.reorder_unit,
+                i.inventory_description,
+                json_agg(v.*) AS vendors,
+                i.created_at,
+                i.updated_at
+            FROM item i
+            LEFT JOIN product_category pc ON i.product_category = pc.id
+            LEFT JOIN item_preferred_vendor ipv ON i.id = ipv.item_id
+            LEFT JOIN vendor v ON ipv.vendor_id = v.id
+            LEFT JOIN units pu ON pu.id = CASE 
+                                            WHEN i.unit_category IN ('mass', 'volume') 
+                                            THEN i.product_units::UUID
+                                            ELSE NULL
+                                        END -- Join only if UUID
+            LEFT JOIN units uu ON uu.id = CASE 
+                                            WHEN i.unit_category IN ('mass', 'volume') 
+                                            THEN i.usage_unit::UUID
+                                            ELSE NULL
+                                        END -- Join only if UUID
+            WHERE i.id = $1
+            GROUP BY i.id, pc.name, pu.unit, uu.unit
+        `;
+
+        const item = await pool.query(itemQuery, [itemId]);
+
+        if (item.rows.length === 0) {
+            return responseSender(res, 404, false, "Item not found");
+        }
+
+        return responseSender(res, 200, true, "Item fetched", item.rows[0]);
+    } catch (error) {
+        next(error);
+    }
+};
+
+const updateItem = async (req, res, next) => {
+    const itemId = req.query.id; // Assuming the item ID is provided as a query parameter
+    const {
+        type,
+        name,
+        product_category,
+        unit_category,
+        quantity_units,
+        product_units,
+        product_catalog,
+        usage_unit,
+        image,
+        vendor_ids,
+        description,
+        stock_in_hand,
+        opening_stock_rate,
+        reorder_unit,
+        inventory_description
+    } = req.body;
+
+    try {
+        // Fetch existing item to check if it exists
+        const existingItem = await pool.query(
+            `SELECT * FROM item WHERE id = $1`,
+            [itemId]
+        );
+
+        if (existingItem.rows.length === 0) {
+            return responseSender(res, 404, false, "Item not found");
+        }
+
+        if (type && type !== 'PRODUCT' && type !== 'SERVICE') {
+            return responseSender(res, 422, false, "Invalid type. Must be PRODUCT or SERVICE.");
+        }
+
+        // Construct the update query dynamically
+        let updateQuery = 'UPDATE item SET ';
+        let queryParams = [];
+        let index = 1;
+
+        // Build update query based on provided fields
+        if (type) {
+            updateQuery += `type = $${index}, `;
+            queryParams.push(type);
+            index++;
+        }
+        if (name) {
+            updateQuery += `name = $${index}, `;
+            queryParams.push(name);
+            index++;
+        }
+        if (product_category) {
+            updateQuery += `product_category = $${index}::uuid, `;
+            queryParams.push(product_category);
+            index++;
+        }
+        if (unit_category) {
+            updateQuery += `unit_category = $${index}, `;
+            queryParams.push(unit_category);
+            index++;
+        }
+        if (quantity_units) {
+            updateQuery += `quantity_units = $${index}, `;
+            queryParams.push(quantity_units);
+            index++;
+        }
+        if (product_units) {
+            updateQuery += `product_units = $${index}, `;
+            queryParams.push(product_units);
+            index++;
+        }
+        if (usage_unit) {
+            updateQuery += `usage_unit = $${index}, `;
+            queryParams.push(usage_unit);
+            index++;
+        }
+        if (product_catalog) {
+            updateQuery += `product_catalog = $${index}, `;
+            queryParams.push(product_catalog);
+            index++;
+        }
+        if (image) {
+            updateQuery += `image = $${index}, `;
+            queryParams.push(image);
+            index++;
+        }
+        if (description) {
+            updateQuery += `description = $${index}, `;
+            queryParams.push(description);
+            index++;
+        }
+        if (stock_in_hand) {
+            updateQuery += `stock_in_hand = $${index}, `;
+            queryParams.push(stock_in_hand);
+            index++;
+        }
+        if (opening_stock_rate) {
+            updateQuery += `opening_stock_rate = $${index}, `;
+            queryParams.push(opening_stock_rate);
+            index++;
+        }
+        if (reorder_unit) {
+            updateQuery += `reorder_unit = $${index}, `;
+            queryParams.push(reorder_unit);
+            index++;
+        }
+        if (inventory_description) {
+            updateQuery += `inventory_description = $${index}, `;
+            queryParams.push(inventory_description);
+            index++;
+        }
+
+        // Remove trailing comma and space from the query
+        updateQuery = updateQuery.slice(0, -2);
+
+        // Add WHERE clause for the item ID
+        updateQuery += ' WHERE id = $' + index;
+        queryParams.push(itemId);
+
+        // Perform the update operation
+        await pool.query(updateQuery, queryParams);
+
+        // Update the preferred vendors if vendor_ids is provided
+        if (vendor_ids && vendor_ids.length > 0) {
+            // Delete existing preferred vendors for the item
+            await pool.query('DELETE FROM item_preferred_vendor WHERE item_id = $1', [itemId]);
+
+            // Insert new preferred vendors
+            for (const vendorId of vendor_ids) {
+                await pool.query(
+                    'INSERT INTO item_preferred_vendor (item_id, vendor_id) VALUES ($1::uuid, $2::uuid)',
+                    [itemId, vendorId]
+                );
+            }
+        }
+
+        // Fetch updated item details
+        const updatedItemDetails = await pool.query(
+            `SELECT 
+                i.id,
+                i.type,
+                i.name AS name,
+                i.unit_category,
+                i.product_catalog,
+                i.description,
+                i.image, 
+                i.stock_in_hand,
+                i.opening_stock_rate,
+                i.reorder_unit,
+                i.inventory_description,
+                pc.name AS product_category,
+                i.quantity_units,
+                u1.unit AS product_units,
+                u2.unit AS usage_unit,
+                json_agg(v.*) AS vendors,
+                i.created_at,
+                i.updated_at
+            FROM item i
+            LEFT JOIN product_category pc ON i.product_category = pc.id::uuid
+            LEFT JOIN units u1 ON i.product_units = u1.unit
+            LEFT JOIN units u2 ON i.usage_unit = u2.unit
+            LEFT JOIN item_preferred_vendor ipv ON i.id = ipv.item_id::uuid
+            LEFT JOIN vendor v ON ipv.vendor_id = v.id::uuid
+            WHERE i.id = $1
+            GROUP BY i.id, pc.name, u1.unit, u2.unit`,
+            [itemId]
+        );
+
+        return responseSender(res, 200, true, "Item Updated", updatedItemDetails.rows[0]);
+    } catch (error) {
+        next(error);
+    }
+}
+
+const deleteItem = async (req, res, next) => {
+    const itemId = req.query.id;
+
+    try {
+        // Check if the item exists
+        const existingItem = await pool.query(
+            `SELECT * FROM item WHERE id = $1`,
+            [itemId]
+        );
+
+        if (existingItem.rows.length === 0) {
+            return responseSender(res, 404, false, "Item not found");
+        }
+
+        // Delete related records in item_preferred_vendor table
+        await pool.query(
+            `DELETE FROM item_preferred_vendor WHERE item_id = $1`,
+            [itemId]
+        );
+
+        // Delete the item
+        await pool.query(
+            `DELETE FROM item WHERE id = $1`,
+            [itemId]
+        );
+
+        return responseSender(res, 200, true, "Item deleted successfully");
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = {
+    createItem,
+    itemList,
+    specifiItem,
+    updateItem,
+    deleteItem
+};
